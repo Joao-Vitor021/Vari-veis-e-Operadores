ab262b4c7bac07f09b83b4a2bed21f8d227d4824
joaov <joaov@DESKTOP-C6GB8J9.mshome.net>
2026-01-05 22:01:48 -0300

    Primeiro commit

---
+++ Projetos/src/PostLinkedln/VariaveisEOperadores.java
@@ -0,0 +1,14 @@
+package PostLinkedln;
+
+public class VariaveisEOperadores {
+	public static void main(String[] args) {
+		double n1 = 25;
+		double n2 = 5;
+		
+		System.out.printf("A SOMA entre %.1f e %.1f é igual a %.1f\n", n1, n2, (n1+n2));
+		System.out.printf("A SUBTRAÇÃO entre %.1f e %.1f é igual a %.1f\n", n1, n2, (n1-n2));
+		System.out.printf("A MULTIPLICAÇÃO entre %.1f e %.1f é igual a %.1f\n", n1, n2, (n1*n2));
+		System.out.printf("A DIVISÃO entre %.1f e %.1f é igual a %.1f", n1, n2,n1/n2);
+	}
+
+}
